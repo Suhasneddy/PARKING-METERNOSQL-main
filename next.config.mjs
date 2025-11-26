8148cc551f6010a84de88d82fbc4b30d69321ece
Suhasneddy <snr200564@gmail.com>
2025-11-26 09:41:03 +0530

    local changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
 export default nextConfig
